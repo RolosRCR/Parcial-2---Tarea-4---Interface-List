5cc68d6fcd85bdf6596424a91a265e1cc264594a
RolosRCR <a286847>
2021-05-16 11:20:45 -0500

    P3T4

--- test/com/company/list/ArrayListTest.java
+++ test/com/company/list/ArrayListTest.java
@@ -10,55 +10,55 @@ public class ArrayListTest {
     @Test
     public void whenArrayListIsCreated_thenSizeIsZero() {
 
-        //inicializacion
+        //inicializacion:
         ArrayList<Integer> List = new ArrayList<>();
 
-        //Ejecucion
+        //Ejecución:
         int size = List.getSize();
 
-        //Validacion
+        //Validación:
         assertEquals(0,size,"size no es 0");
     }
 
     @Test
-    public void givenANewArray_WhenAddElement_ThenSizeIsOne() {
+    public void givenANewArray_whenAddElement_thenSizeIsOne() {
 
-        //Inicialización
+        //Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
 
-        //Ejecución
+        //Ejecución:
         List.add(500);
 
-        //Validación
+        //Validación:
         assertEquals(List.getSize(),1);
     }
     @Test
-    public void givenAnExistentArrayWithOneElement_WhenAddElement_ThenSizeIsTwo() {
+    public void givenAnExistentArrayWithOneElement_whenAddElement_thenSizeIsTwo() {
 
-        //Inicialización
+        //Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
         List.add(500);
 
-        //Ejecución
+        //Ejecución:
         List.add(100);
 
-        //Validación
+        //Validación:
         assertEquals(List.getSize(),2);
         assertEquals(500,List.get(0));
         assertEquals(100,List.get(1));
     }
     @Test
-    public void givenAnExistentArrayWithNoCapacity_WhenAddElement_ThenSizeIncrementsAndCapacityIncrements() {
+    public void givenAnExistentArrayWithNoCapacity_whenAddElement_thenSizeIncrementsAndCapacityIncrements() {
 
-        //Inicialización
+        //Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
         List.add(100);
         List.add(200);
 
-        //Ejecución
+        //Ejecución:
         List.add(300);
 
-        //Validación
+        //Validación:
         assertEquals(3,List.getSize(),"Deberia tener 3 elementos");
         assertEquals(100,List.get(0));
         assertEquals(200,List.get(1));
@@ -67,16 +67,16 @@ public class ArrayListTest {
     @Test
     public void givenAnExistentArrayWithElements_WhenIterator_ThenAllElementsAreAccessible() {
 
-        //Inicialización
+        //Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
         List.add(100);
         List.add(200);
         List.add(300);
         List.add(400);
         List.add(500);
-        //Ejecución
+        //Ejecución:
         Iterator<Integer> it=List.getIterator();
-        //Validación
+        //Validación:
         assertTrue(it.hasNext());
         assertEquals(100,it.next());
         assertTrue(it.hasNext());
@@ -91,16 +91,16 @@ public class ArrayListTest {
     @Test
     public void givenAnExistentArrayWithElements_WhenReverseIterator_ThenAllElementsAreAccesible() {
 
-        //Inicialización
+        //Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
         List.add(100);
         List.add(200);
         List.add(300);
         List.add(400);
         List.add(500);
-        //Ejecución
+        //Ejecución:
         Iterator<Integer> it=List.getReverseIterator();
-        //Validación
+        //Validación:
         assertTrue(it.hasNext());
         assertEquals(500,it.next());
         assertTrue(it.hasNext());
@@ -115,16 +115,16 @@ public class ArrayListTest {
     @Test
     public void givenAnExistentArrayWithFourElements_WhenDeleteElementHead_ThenSizeDecrements() throws MyIndexOutOfBoundsException {
 
-        //Inicialización
+        //Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
         List.add(100);
         List.add(200);
         List.add(300);
         List.add(400);
-        //Ejecución
+        //Ejecución:
         List.delete(0);
 
-        //Validación
+        //Validación:
         assertEquals(3,List.getSize());
         assertEquals(200,List.get(0));
         assertEquals(300,List.get(1));
@@ -133,39 +133,89 @@ public class ArrayListTest {
     @Test
     public void givenAnExistentArrayWithFourElements_WhenDeleteElementTail_ThenSizeDecrements() {
 
-        //Inicialización
+        //Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
         List.add(100);
         List.add(200);
         List.add(300);
         List.add(400);
-        //Ejecución
+
+        //Ejecución:
         List.delete(3);
 
-        //Validación
+        //Validación:
         assertEquals(3,List.getSize());
         assertEquals(100,List.get(0));
         assertEquals(200,List.get(1));
         assertEquals(300,List.get(2));
     }
     @Test
-    public void givenAnExistentArrayWithFourElements_whenDeleteIndexDoesNotExist_ThenIndexOutOfExceptionIsThrown() {
+    public void givenAnExistentArrayWithElements_whenDeleteIndexDoesNotExist_ThenIndexOutOfExceptionIsThrown() {
 
-        //Inicialización
+//        Inicialización:
         ArrayList<Integer> List = new ArrayList<>();
         List.add(100);
         List.add(200);
         List.add(300);
         List.add(400);
-        //Ejecución
-
-        //Validación
+//Forma alternartiva
 //        try{
 //            List.delete(5);
 //            Assertions.fail("MyIndexOutOfBoundsException should be thrown");
 //        }catch(MyIndexOutOfBoundsException ex){
 //
 //        }
+
+        //Validación+Ejecución:
         Assertions.assertThrows(MyIndexOutOfBoundsException.class,()->List.delete(5));
+    }
+    @Test
+    public void givenAnExistentArrayWithElements_whenGet_thenAllElementsAreAccessible() {
+
+        //Inicialización:
+        ArrayList<Integer> List = new ArrayList<>();
+        List.add(100);
+        List.add(200);
+        List.add(300);
+        List.add(400);
+        //Ejecución:
+
+        //Validación:
+        Assertions.assertThrows(MyIndexOutOfBoundsException.class,()->List.get(5));
+    }
+    @Test
+    public void givenAnExistentArrayWithElements_whenGetIndexDoesNotExist_thenIndexOutOfExceptionIsThrown() {
+
+        //Inicialización:
+        ArrayList<Integer> List = new ArrayList<>();
+        List.add(100);
+        List.add(200);
+        List.add(300);
+        List.add(400);
+        //Ejecución+Validación:
+        assertEquals(100,List.get(0));
+        assertEquals(200,List.get(1));
+        assertEquals(300,List.get(2));
+        assertEquals(400,List.get(3));
+    }
+    @Test
+    public void givenAnExistentArrayWithElements_whenInsetrs_thenArrayListKeepsTheSame() {
+
+        //Inicialización:
+        ArrayList<Integer> List = new ArrayList<>();
+        List.add(100);
+        List.add(200);
+        List.add(300);
+        List.add(400);
+
+        //Ejecución
+        Iterator<Integer> it=List.getIterator();
+        List.insert(200,Position.AFTER,it);
+
+        // Validación:
+        assertEquals(100,List.get(0));
+        assertEquals(200,List.get(1));
+        assertEquals(300,List.get(2));
+        assertEquals(400,List.get(3));
     }
 }
